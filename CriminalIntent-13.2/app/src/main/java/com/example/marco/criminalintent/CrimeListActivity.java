b7fee0be9ec90180c913140d032f3709dc71ca4c
markon <marcusbu@gmail.com>
2016-04-16 12:41:52 +0200

    Added skeleton for challenge 13.2

---
+++ CriminalIntent-13.2/app/src/main/java/com/example/marco/criminalintent/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package com.example.marco.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
