b1effbb913453c212c006dc171ac1bb09ecc42ff
markon <marcusbu@gmail.com>
2016-08-17 10:47:20 +0200

    Added skeleton for solution 15.2

---
+++ CriminalIntent-15.2/app/src/main/java/com/example/marco/criminalintent/Crime.java
@@ -0,0 +1,58 @@
+package com.example.marco.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private boolean mSolved;
+    private String mSuspect;
+    private Date mDate;
+
+    public Crime() {
+        this(UUID.randomUUID());
+    }
+
+    public Crime(UUID id) {
+        mId = id;
+        mDate = new Date();
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getSuspect() {
+        return mSuspect;
+    }
+
+    public void setSuspect(String suspect) {
+        mSuspect = suspect;
+    }
+}
